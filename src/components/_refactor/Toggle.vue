6302e5f5a270b099f8ea242a96c4a80841d5ef25
Bi Wu <alerk_star@outlook.com>
2022-06-01 23:01:32 -0400

    added option switches

---
+++ src/components/_refactor/Toggle.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  label: String,
+  value: Boolean,
+  class: String,
+});
+defineEmits(['toggle']);
+</script>
+<template>
+  <div class="flex items-center" :class="props.class">
+    <label class="mr-3">{{ props.label }}</label>
+    <n-switch :value="props.value" @click="$emit('toggle')" />
+  </div>
+</template>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,38 @@
-<template>
-    <div class="dashboard">
-        <div class="w-full p-4 ">
+<script setup>
+import { ref } from 'vue';
+import Toggle from '@/components/_refactor/Toggle.vue';
 
-        </div>
+const options = ref({
+  botScanRate: {
+    label: 'Bot Scan Rate',
+    value: true
+  },
+  matchKeyword: {
+    label: 'Match Keyword',
+    value: false,
+  },
+  qrScan: {
+    label: 'QR Scan',
+    value: true,
+  },
+  blacklist: {
+    label: 'Blacklist',
+    value: true,
+  },
+});
+</script>
+<template>
+  <div class="dashboard">
+    <div class="w-full p-4 ">
+      <h3>Dashboard</h3>
+      <div class="flex justify-center">
+        <Toggle
+          v-for="(option, key, index) in options"
+          :key="key"
+          :class="index === 0 ? 'ml-0' : 'ml-6'"
+          v-bind="option"
+          @toggle="options[key].value = !options[key].value" />
+      </div>
     </div>
+  </div>
 </template>
