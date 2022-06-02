2e7b24c3160cd64e19007cfb1ffe55873365d47d
Bi Wu <alerk_star@outlook.com>
2022-06-01 23:15:25 -0400

    added stats boxes

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -33,6 +33,37 @@ const options = ref({
           v-bind="option"
           @toggle="options[key].value = !options[key].value" />
       </div>
+      <div class="stats">
+        <div class="stats__item">
+          <h4 class="stats__item-title mb-6">Accounts</h4>
+          <span class="stats__item-number mr-2">4</span><span class="text-sm">Active accounts</span>
+        </div>
+        <div class="stats__item">
+          <h4 class="stats__item-title mb-6">Tweets</h4>
+          <span class="stats__item-number">19</span>
+        </div>
+        <div class="stats__item">
+          <div class="flex items-center justify-between mb-6">
+            <h4 class="stats__item-title">Monitored Acccounts</h4>
+            <button class="bg-background_dark px-4 py-1 rounded-sm text-xs">Edit</button>
+          </div>
+          <span class="stats__item-number">11</span>
+        </div>
+      </div>      
     </div>
   </div>
 </template>
+<style lang="scss" scoped>
+.stats {
+  @apply flex items-center gap-[1rem] mt-8;
+  &__item {
+    @apply bg-gray-800 p-4 rounded-md flex-1;
+    &-title {
+      @apply font-bold;
+    }
+    &-number {
+      @apply text-cyan-400 text-2xl;
+    }
+  }
+}
+</style>
